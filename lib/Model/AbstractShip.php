a5cf4873cfb408172f00ebd8658ec0af4dd1c22d
petrero <petrero@gmail.com>
2017-03-26 12:04:42 +0300

    But first, Exception is a core PHP class, and when we added a namespace to this file, we forget to change it to \Exception.
    That's better. Now refresh again. This is a much better error:
    
    Uncaught Exception: Invalid strength passed: 'banana'
    When things go Wrong: Throw an Exception
    
    When things go wrong, we throw exceptions. Why? Well, first: it stops execution of the page and immediately shows us a nice error.
    
     Tip
    If you install the XDebug extension, exception messages are more helpful, prettier.

--- lib/Model/AbstractShip.php
+++ lib/Model/AbstractShip.php
@@ -39,7 +39,7 @@ abstract class AbstractShip {
 
   public function setStrength($number){
     if (!is_numeric($number)) {
-      throw new Exception('Strength must be a number, duh!');
+      throw new \Exception('Strength must be a number, duh!');
     }
 
     $this->strength = $number;

11a56f72dcb14ba9925be8e4ff07ed1cf3f858ef
petrero <petrero@gmail.com>
2017-03-26 12:35:47 +0300

    Don't Get Too Clever
    Oftentimes, I see people try to not throw exceptions. Instead, they try to recover in some way. Don't do that. I would rather throw an exception, see the error in my error log and fix it than try to render a broken page and never realize that there's a bug in my code.
    
    In fact, most frameworks have a pretty easy way to automatically notify you - like via Slack or by email - whenever an exception is thrown on your site.
    
    Let's fix our code: take out the throw new exception and change the table back to ship. All better.

--- lib/Service/PdoShipStorage.php
+++ lib/Service/PdoShipStorage.php
@@ -9,8 +9,7 @@ class PdoShipStorage implements ShipStorageInterface {
   }
 
   public function fetchAllShipsData(){
-    throw new \InvalidArgumentException('something else went wrong');
-    $statement = $this->pdo->prepare('SELECT * FROM FOOOD');
+    $statement = $this->pdo->prepare('SELECT * FROM ship');
     $statement->execute();
 
     return $statement->fetchAll(\PDO::FETCH_ASSOC);

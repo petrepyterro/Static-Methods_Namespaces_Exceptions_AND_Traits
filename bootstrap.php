aa232b3842e7554ff62462ccc118d70f8b33206b
petrero <petrero@gmail.com>
2017-03-26 11:23:40 +0300

    Back in your terminal, run:
    composer install
    
    This command normally downloads any external packages that we need - but we haven't defined any. But it also generates some autoload files inside a new vendor/ directory.
    
    To use those, open bootstrap.php, delete all the manual autoload stuff, and replace it with just require __DIR__vendor/autoload.php, which is one of the files that composer just generated.
    
    That's it.
    
    You also usually don't commit the vendor/ directory to your Git repository: team members just run this same command when they download the project.
    
    Let's see if it works! Go back and refresh! It does! And as we add more classes and more directories to lib/, everything will keep working. AND, if you guys want to start downloading external libraries into your project via Composer, you can do that too and immediately reference those classes without needing to worry about require statements or autoloaders. Composer takes care of everything.

--- bootstrap.php
+++ bootstrap.php
@@ -7,10 +7,4 @@ $configuration = array(
 );
 
 
-spl_autoload_register(function($className){
-  $path = __DIR__.'/lib/'.str_replace('\\', '/', $className).'.php';
-  
-  if(file_exists($path)){
-    require $path;
-  }
-});
+require __DIR__.'/vendor/autoload.php';

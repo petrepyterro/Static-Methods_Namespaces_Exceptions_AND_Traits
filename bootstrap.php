7db538a4fe2026c08b42ecbd2405d5b56cba06ee
petrero <petrero@gmail.com>
2017-03-26 10:57:38 +0300

    Copy code from branch 7

--- bootstrap.php
+++ bootstrap.php
@@ -1,19 +1,16 @@
 <?php
 
 $configuration = array(
-  'db_dsn'  => 'mysql:host=localhost;dbname=oo_battle',
+  'db_dsn'  => 'mysql:host=localhost;dbname=Services_Dependency_Injection_AND_Containers',
   'db_user' => 'root',
-  'db_pass' => null,
+  'db_pass' => 'mysql',
 );
 
-require_once __DIR__.'/lib/Service/Container.php';
-require_once __DIR__.'/lib/Model/AbstractShip.php';
-require_once __DIR__.'/lib/Model/Ship.php';
-require_once __DIR__.'/lib/Model/RebelShip.php';
-require_once __DIR__.'/lib/Model/BrokenShip.php';
-require_once __DIR__.'/lib/Service/BattleManager.php';
-require_once __DIR__.'/lib/Service/ShipStorageInterface.php';
-require_once __DIR__.'/lib/Service/PdoShipStorage.php';
-require_once __DIR__.'/lib/Service/JsonFileShipStorage.php';
-require_once __DIR__.'/lib/Service/ShipLoader.php';
-require_once __DIR__.'/lib/Model/BattleResult.php';
+
+spl_autoload_register(function($className){
+  $path = __DIR__.'/lib/'.str_replace('\\', '/', $className).'.php';
+  
+  if(file_exists($path)){
+    require $path;
+  }
+});
--- lib/Service/ShipStorageInterface.php
+++ lib/Service/ShipStorageInterface.php
@@ -1,4 +1,5 @@
 <?php
+namespace Service;
 
 interface ShipStorageInterface {
   /**

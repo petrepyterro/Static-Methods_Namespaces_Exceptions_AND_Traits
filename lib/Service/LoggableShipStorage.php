038da94393832b135f8dd2d23ce54328351896b4
petrero <petrero@gmail.com>
2017-03-28 20:39:36 +0300

    But LoggableShipStorage will not actually do any of the ship-loading work - it'll offload all that hard work to some other ship storage object, like PDOShipStorage. To do that, add a new private {dollar_sign}shipStorage property and a public function __construct() method that accepts one ShipStorageInterface argument. Then, set that value onto the {dollar_sign}shipStorage property.
    For fetchAllShipData(), just return {dollar_sign}this->shipStorage->fetchAllShipsData(). Repeat for the other method: return {dollar_sign}this->shipStorage->fetchSingleShipData().
    
    We've now created a wrapper object that offloads all of the work to an internal ship storage object. This is composition: you put one object inside of another.

--- lib/Service/LoggableShipStorage.php
+++ lib/Service/LoggableShipStorage.php
@@ -1,13 +1,20 @@
 <?php
 namespace Service;
 
-class LoggableShipStorage implements Service\ShipStorageInterface{
+class LoggableShipStorage implements ShipStorageInterface{
+  private $shipStorage;
+  
+  public function __construct(ShipStorageInterface $shipStorage) {
+    return $this->shipStorage = $shipStorage;
+  }
+
+
   public function fetchAllShipsData() {
-    
+    return $this->shipStorage->fetchAllShipsData();
   }
   
   public function fetchSingleShipData($id) {
-    
+    $this->shipStorage->fetchSingleShipData($id);  
   }
 }
 
